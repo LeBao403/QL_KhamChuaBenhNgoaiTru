0ce4b4cb99bf9742f054fd9b7578f64f83cc0c5a
LeBao403 <lebaosg@gmail.com>
2026-03-19 14:05:24 +0700

    Upgrades: Upgraded homepage view, show view of doctors and specialties. Added Images section and database.

---
+++ QL_KhamChuaBenhNgoaiTru/Models/PhieuKhamSangLoc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QL_KhamChuaBenhNgoaiTru.Models
+{
+    public class PhieuKhamSangLoc
+    {
+        [Key]
+        public int MaPhieuKhamSL { get; set; }
+        public decimal? ChieuCao { get; set; }
+        public decimal? CanNang { get; set; }
+        public int? Mach { get; set; }
+        public string HuyetAp { get; set; }
+        public int? NhipTho { get; set; }
+        public string KetLuan { get; set; }
+        public string GhiChu { get; set; }
+        public string MaBacSiKham { get; set; }
+        public int? MaPhong { get; set; }
+    }
+}
